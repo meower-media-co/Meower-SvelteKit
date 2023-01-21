f086126521e25bfeeb7ddb212b702748214faee1
ShowierData9978 <mellfang36@gmail.com>
2023-01-21 14:08:31 -0600

    uhm

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,35 +1,3 @@
-/**
- * A CL Turbo packet.
- */
-export interface Packet {
-	cmd: string;
-	val: string | object;
-	payload?: string | object;
-	listener?: string;
-}
+// what is the purpose of this file?
 
-export interface ListenerPacket extends Packet {
-	listener: string;
-}
-
-export interface ModePacket extends Packet {
-	val: {
-		mode: string;
-		payload: any;
-	};
-}
-
-/**
- * The value returned by sendListener().
- */
-export interface SendListenerReturn {
-	packets: Packet[];
-	statuscode: string | null;
-	ok: boolean;
-}
-
-export interface ModeRequestReturn {
-	payload: any;
-	statuscode: string | null;
-	ok: boolean;
-}
+export {};
